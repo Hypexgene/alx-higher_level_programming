087fb45ed8433c38d539f43f73949a26bdfb92c8
Hypexgene <ashtonmuiru@gmail.com>
2023-11-08 13:03:40 -0800

    committashton

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -22,5 +22,3 @@ if __name__ == "__main__":
             print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
         elif sys.argv[2] == "/":
             print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
-
-
--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -10,4 +10,3 @@ def magic_calculation(a, b):
         return (c)
     else:
         return sub(a, b)
-
