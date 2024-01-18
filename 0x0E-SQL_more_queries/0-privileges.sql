1f3a242923f7f0ec489c62f0ab6371b4325e89a6
Hypexgene <ashtonmuiru@gamil.com>
2024-01-18 11:11:09 -0800

    ashtontasks

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,9 +1,3 @@
 -- Lists all privileges of the MySQL users user_0d_1 and user_0d_2
-GRANT SHOW DATABASES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
-
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
-
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
